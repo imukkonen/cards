b936837450c35cf4e1b0eb1bd6ca396d34ef02eb
Irina Mukkonen <irina.mukkonen@myy.haaga-helia.fi>
2019-09-24 21:55:00 +0300

    adding connect.php

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+function connect_db() {
+
+  $servername = "localhost";
+  $username = "a1702617";
+  $password = "tiXAI556o";
+  $dbname = "a1702617";
+
+  // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  } else {
+  }
+  return $conn; 
+}
+?>
